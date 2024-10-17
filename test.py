aa0c916aa93ca54995ccdb32f6a1b0ca37fe2e14
TaiseiShimoyama <83171672+TaiseiShimoyama@users.noreply.github.com>
2024-10-17 17:07:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from IPython.display import display, Markdown
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
